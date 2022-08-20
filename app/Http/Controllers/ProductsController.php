8b3cd3158772f8759d2e5d020ca9f25afc1b6fe0
shirinkalam <mahdishiri869@gmail.com>
2022-08-20 11:50:51 +0430

    SessionStorage implemented

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Support\Storage\Contracts\StorageInterface;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
-        return view('products',compact('products'));
+        $sessionStorage = resolve(StorageInterface::class);
+
+        $sessionStorage->set('product',1);
+        $sessionStorage->set('item',1);
+        $sessionStorage->set('test',1);
+        $sessionStorage->unset('item');
+
+        dd($sessionStorage->count());
+
+        // dd($sessionStorage->all());
+
+        // $products = Product::all();
+
+        // return view('products',compact('products'));
     }
 }
